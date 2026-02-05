e57bea2fbacff7be049af4b478a83f387c2a7d75
alin26 <alin.joseph.shan25@gmail.com>
2026-02-05 20:21:17 +0530

    Step 4 (fix):Enabled Immersive mode because of pill touch issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'views/home_view.dart';
 
-void main(){runApp(const MyApp());}
+void main(){runApp(const MyApp());
+WidgetsFlutterBinding.ensureInitialized();
+SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);}
 
 
 class MyApp extends StatelessWidget {
