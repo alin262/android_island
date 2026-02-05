bf68989cb043e75b808ffb2bde6f6b3926424d72
alin26 <alin.joseph.shan25@gmail.com>
2026-02-05 20:08:30 +0530

    Added tap to expand anitmation

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Android Island",
       home: HomeView(),
+      
     );
   }
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -8,26 +8,40 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  bool isExpanded= false;
+  void isToggled(){
+    setState(() {
+      isExpanded=!isExpanded;
+      print(isExpanded);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          const Center(child: Text("app content"),),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Padding(
-              padding: const EdgeInsets.all(7.0),
-              child: Container(
-                width: 120,height: 35,decoration: BoxDecoration(
-                  color: Colors.black,borderRadius: BorderRadius.circular(20)
+      backgroundColor: Colors.blueGrey,
+      body: 
+          Stack(
+            children: [
+              const Center(child: Text("app content"),),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(7.0),
+                  child: GestureDetector(
+                    onTap: isToggled,
+                    child: AnimatedContainer(
+                      duration: const Duration(seconds: 1),curve: Curves.easeInOutBack,
+                      width:isExpanded? 120:300,height:isExpanded? 35:150,decoration: BoxDecoration(
+                        color: Colors.black,borderRadius: BorderRadius.circular(isExpanded ? 30 : 20)
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          )
-        ],
-      )
+              )
+            ],
+          ),
+        
     );
   }
 }
